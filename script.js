cd7b5cc5986b864e36705c0564f587d33bd818e8
Ketan-01 <98932213+Ketan-01@users.noreply.github.com>
2022-02-20 20:04:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer","Programmer", "Music lover"],
+        strings: ["Developer.","Programmer.", "Music lover."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Programmer", "Music lover"],
+        strings: ["Developer.", "Programmer.", "Music lover."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
